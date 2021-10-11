6943c404873754bfa603f93b7b61e5dccabbd7e0
reyhannaufal <reyhannaufal21@gmail.com>
2021-10-11 14:27:20 +0700

    [ADD] threejs section

---
+++ threejs/lights/rootLights.js
@@ -0,0 +1,24 @@
+import * as THREE from "three";
+
+export const ambientLight = new THREE.AmbientLight(
+  new THREE.Color(0xffffff),
+  0.51
+);
+
+export const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
+directionalLight.position.set(1, 0.25, 0);
+
+export const hemisphereLight = new THREE.HemisphereLight(
+  0xff0000,
+  0x000ff,
+  0.3
+);
+
+export const pointLight = new THREE.PointLight(0xff90000, 0.5, 10, 2);
+pointLight.position.set(2, -0.5, 1);
+
+export const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.lookAt(new THREE.Vector3(-2, 0, 1.5));
+
+export const spotLight = new THREE.SpotLight(0x78ff00, 1, 100, Math.PI / 4, 1);
+spotLight.position.set(2, 0, 0);
---
+++ threejs/src/script.js
@@ -0,0 +1,156 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import {
+  ambientLight,
+  directionalLight,
+  hemisphereLight,
+  rectAreaLight,
+  spotLight,
+} from "../lights/rootLights";
+
+const canvas = document.querySelector("canvas.app");
+
+const scene = new THREE.Scene();
+
+/*
+ * Textures
+ */
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMapTexture = cubeTextureLoader.load([
+  "/textures/environmentMaps/1/px.jpg",
+  "/textures/environmentMaps/1/nx.jpg",
+  "/textures/environmentMaps/1/py.jpg",
+  "/textures/environmentMaps/1/ny.jpg",
+  "/textures/environmentMaps/1/pz.jpg",
+  "/textures/environmentMaps/1/nz.jpg",
+]);
+
+scene.background = environmentMapTexture;
+
+/*
+ * Lights
+ */
+scene.add(
+  ambientLight,
+  directionalLight,
+  hemisphereLight,
+  rectAreaLight,
+  spotLight
+);
+
+/*
+ * Object
+ */
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(20, 3, 10, 100),
+  new THREE.MeshBasicMaterial({
+    color: new THREE.Color("pink"),
+  })
+);
+torus.position.z = -10;
+
+const cylinder = new THREE.Mesh(
+  new THREE.CylinderGeometry(0.5, 0.5, 1, 32),
+  new THREE.MeshNormalMaterial({
+    flatShading: true,
+  })
+);
+cylinder.position.x = 0.2;
+cylinder.position.y = 0.8;
+
+const cone = new THREE.Mesh(
+  new THREE.ConeGeometry(0.5, 1, 32),
+  new THREE.MeshStandardMaterial({
+    metalness: 0.7,
+    roughness: 0.5,
+    envmap: environmentMapTexture,
+  })
+);
+cone.position.x = 2;
+cone.position.y = 0.8;
+
+const octhaHedron = new THREE.Mesh(
+  new THREE.OctahedronGeometry(1, 0),
+  new THREE.MeshStandardMaterial({
+    color: 0x49ef4,
+    wireframe: true,
+  })
+);
+octhaHedron.position.x = -2;
+octhaHedron.position.y = 0.8;
+
+const floor = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(10, 10),
+  new THREE.MeshToonMaterial({
+    depthTest: true,
+    depthWrite: true,
+  })
+);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.y = -0.65;
+
+scene.add(torus, cylinder, cone, octhaHedron, floor);
+
+/*
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/*
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height);
+camera.position.z = 3.5;
+scene.add(camera);
+
+/*
+ * Control
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+window.addEventListener("resize", () => {
+  //update size
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //update canvas
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+function animate() {
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+
+  cylinder.rotation.x += 0.01;
+  cylinder.rotation.y += 0.01;
+
+  cone.rotation.x += 0.01;
+  cone.rotation.y += 0.01;
+  cone.rotation.z += 0.01;
+
+  octhaHedron.rotation.x += 0.01;
+  octhaHedron.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
